6ce521cebe390313d9c81ddf5e39ee9327bc4efa
Jay Iyer <jiyer1@swarthmore.edu>
2025-06-06 10:20:33 -0400

    added a function in binarithTests that generates all the N-bit binary numbers

--- bases/rsptx/interactives/runestone/numconv/unittests/binarithTest.js
+++ bases/rsptx/interactives/runestone/numconv/unittests/binarithTest.js
@@ -2,25 +2,35 @@
 // this doewsn't work
 let FourBitNums = []
 
-generateAllMatchings = (arr1, arr2, func) => {
+generateAllMatchings = (arr1, arr2) => {
+    target = [];
     arr1.forEach(e1 => {
         arr2.forEach(e2 => {
-            func(e1, e2)
+            if(e1.length > 1 && e2.length > 1){
+                target.push([...e1,...e2])
+            }
+            else if(e1.length > 1){
+                target.push([...e1, e2])
+            }
+            if(e2.length > 1){
+                target.push([e1,...e2])
+            }
+            else{
+                target.push([e1,e2])
+            }
+            
         })
     });
-}
-joinAndPush = (o1, o2) => {
-    target = FourBitNums
-    if(o1.length > 1 && o2.length > 1){
-        target.push([...o1,...o2])
-    }
-    else if(o1.length > 1){
-        target.push([...o1, o2])
-    }
-    if(o2.length > 1){
-        target.push([o1,...o2])
-    }
+    return target;
 }
 
-generateAllMatchings([0,1],[0,1], joinAndPush)
-FourBitNums.forEach(e => console.log(e));
+NBitNums = (n=4) => {
+    let temp = [1,0]
+    for(let i = 0; i < (n-1);  i++){
+        temp = generateAllMatchings([1,0], temp)
+    }
+    return temp
+}
+
+console.log(NBitNums())
+
